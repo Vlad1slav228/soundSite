9f88a7e9f9e57e277ea2f883c39d5f1c6cb64d38
Vlad <myakishev.vladislav@live.ru>
2025-05-11 17:24:43 +0300

    Created an audio player on the Services page

--- src/app/services/complex-processing/page.tsx
+++ src/app/services/complex-processing/page.tsx
@@ -1,15 +1,16 @@
 import "@/styles/reset.scss";
 import Header from "@/components/Header/header";
-import Footer from "@/components/Footer/footer";
-
-import Reviews from "@/components/Reviews/reviews";
 import IntroComplexProcessing from "@/components/ServicesPage/ComplexProcessing/Intro/intro";
+import MusicExamplesServices from "@/components/ServicesPage/MusicExamples/musicExamples";
+import Reviews from "@/components/Reviews/reviews";
+import Footer from "@/components/Footer/footer";
 
 export default function ComplexProcessing() {
   return (
     <>
       <Header />
       <IntroComplexProcessing />
+      <MusicExamplesServices />
       <Reviews />
       <Footer />
     </>
--- src/app/services/vocal-recording/page.tsx
+++ src/app/services/vocal-recording/page.tsx
@@ -1,15 +1,16 @@
 import "@/styles/reset.scss";
 import Header from "@/components/Header/header";
-import Footer from "@/components/Footer/footer";
-
-import Reviews from "@/components/Reviews/reviews";
 import IntroVocalRecording from "@/components/ServicesPage/VocalRecording/Intro/intro";
+import MusicExamplesServices from "@/components/ServicesPage/MusicExamples/musicExamples";
+import Reviews from "@/components/Reviews/reviews";
+import Footer from "@/components/Footer/footer";
 
 export default function VocalRecording() {
   return (
     <>
       <Header />
       <IntroVocalRecording />
+      <MusicExamplesServices />
       <Reviews />
       <Footer />
     </>
