87b8a475db0429b1846daf08f1f8f32a794111cb
Katya Radochina <radochinaekaterina@gmail.com>
2025-06-11 18:59:39 +0300

    adaptive services

--- src/components/MainPage/Services/ServicesCard/servicesCard.tsx
+++ src/components/MainPage/Services/ServicesCard/servicesCard.tsx
@@ -14,17 +14,15 @@ export type ServiceProps = Readonly<{
 export default function ServiceCard({ service }: ServiceProps) {
   return (
     <article className={s.servicesCard}>
-      <div className={s.wrapper}>
         <h3 className={s.servicesCardTitle}>{service.title}</h3>
-      <ul className={s.servicesCardList}>
-        {service.list.map((item, index) => (
-          <li key={index} className={s.servicesCardListItem}>
-            {item}
-          </li>
-        ))}
-      </ul>
-      </div>
-      
+        <ul className={s.servicesCardList}>
+          {service.list.map((item, index) => (
+            <li key={index} className={s.servicesCardListItem}>
+              {item}
+            </li>
+          ))}
+        </ul>
+
       <div className={s.servicesCardFooter}>
         <span className={s.servicesCardPrice}>{service.price}</span>
         <Link href={`/services/${service.slug}`} className="greenButton">
