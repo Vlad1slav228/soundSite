187f2926ddcee0ee0fa0119ab9d10fc14406f09a
Vlad <myakishev.vladislav@live.ru>
2025-06-11 11:59:02 +0300

    added a media requests

--- src/components/MainPage/Hero/hero.tsx
+++ src/components/MainPage/Hero/hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function HeroMain() {
   return (
-    <section className={`${s.heroBlock}`}>
+    <section className={s.heroBlock}>
       <div className={`container ${s.heroContent}`}>
           <h1 className={s.titleMainPage}>{HERO_TITLE}</h1>
           <Image src={MICROPHONE_IMG} alt="Микрофон" className={s.microImg}/>
